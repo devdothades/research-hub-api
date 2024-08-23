c61ff21be4fd324ade8b5633d0cb32b6140ea000
Jai <jeromeinfante0506@gmail.com>
2024-08-23 18:13:13 +0800

    added comments

--- src/auth/TokenMiddleware.js
+++ src/auth/TokenMiddleware.js
@@ -4,21 +4,21 @@ import dotenv from "dotenv";
 dotenv.configDotenv();
 
 const userAuthentication = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader.split(" ")[1];
+    const authHeader = req.headers["authorization"]; // get the token from header
+    const token = authHeader.split(" ")[1]; // removing the "bearer" from the token
 
-    console.log(token);
+    console.log(token); // for development
 
     if (token == null) {
-        return res.sendStatus(401);
+        return res.sendStatus(401); // returns error code if the token is empty or null
     }
 
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
-            return res.sendStatus(401);
+            return res.sendStatus(401); // returns if somethings goes wrong
         }
 
-        req.userId = decoded.userId;
+        req.userId = decoded.userId; // storing the id of the user in
         next();
     });
 };
