f11960d7902a9710d5631ff7f85c78abcf41d9a9
Jai <jeromeinfante0506@gmail.com>
2024-08-23 18:08:37 +0800

    added comments

--- src/auth/TokenMiddleware.js
+++ src/auth/TokenMiddleware.js
@@ -4,20 +4,22 @@ import dotenv from "dotenv";
 dotenv.configDotenv();
 
 const userAuthentication = (req, res, next) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader.split(" ")[1];
+    const authHeader = req.headers["authorization"]; // get the token from headers
+    const token = authHeader.split(" ")[1]; // removing the "bearer" from the token
 
-    console.log(token);
+    console.log(token); // for development
 
     if (token == null) {
-        return res.sendStatus(401);
+        return res.sendStatus(401); // returns error code if the token is null
     }
 
+    // verify the token if it matches with the environment key
     jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
         if (err) {
             return res.sendStatus(401);
         }
 
+        // stores the user id in a userId request header
         req.userId = decoded.userId;
         next();
     });
--- src/researches/controller.js
+++ src/researches/controller.js
@@ -1,9 +1,22 @@
-import pool from "../../model";
+import pool from "../../model.js";
 import asyncHandler from "../utils/asyncHandler.js";
 
-const getResearches = asyncHandler(async (req, res) => {});
+const getResearches = asyncHandler(async (req, res) => {
+    const userId = req.userId;
+
+    try {
+        console.log(userId);
+        console.log("test");
+    } catch (error) {
+        res.status(401).json({ msg: error });
+    }
+});
 const getResearch = asyncHandler(async (req, res) => {});
-const createResearch = asyncHandler(async (req, res) => {});
+const createResearch = asyncHandler(async (req, res) => {
+    const userId = req.userId;
+    const { title, authors, category, strand, description, pdf_name } =
+        req.body;
+});
 const updateResearch = asyncHandler(async (req, res) => {});
 const deleteResearch = asyncHandler(async (req, res) => {});
 
